9ce79ebb0eccdd37b163a0f864d89a8bce26499f
FlorianxK <Florian.Koehler@jokonet.de>
2026-08-15 16:19:55 +0200

    database setup

---
+++ src/main/java/com/dev/movieapi/repositries/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.dev.movieapi.repositries;
+
+import com.dev.movieapi.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
